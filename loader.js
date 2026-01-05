45e0572f67025c82936d061ef4b3aace9d98d98c
Jason Q <59822522+jayquake@users.noreply.github.com>
2026-01-05 10:32:32 +0200

    Add script loader for external app-flow.js

---
+++ loader.js
@@ -0,0 +1,7 @@
+(function () {
+  const s = document.createElement("script");
+  const h = document.querySelector("head") || document.body;
+  s.src = "https://ar-fixes--accesswidget--test.acsb-test.com/app-flow.js";
+  s.async = true;
+  h.appendChild(s);
+})();
